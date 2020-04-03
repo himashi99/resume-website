4bd6d35b7e168d58eecf34a3e5ca34bb78edad8d
Himashi Hettege Dona <himashi99@hotmail.com>
2020-04-03 10:12:13 -0400

    Added uncompressed version of jQuery and testing smooth scroll

--- scripts/script.js
+++ scripts/script.js
@@ -78,6 +78,16 @@ if ($( window ).width() < 600) {
 
 }
 
+// Scrolling
+
+if ($( window ).width() < 600) {
+  $("#skills-button").click(function() {
+    var position = $("#ifrm").offset().top; //calculates how far the top of the Carousel is from the top of the page
+    $('body, html').animate({ scrollTop: position }, 700); });
+  console.log("bonjour");
+}
+
+
 
 
 
